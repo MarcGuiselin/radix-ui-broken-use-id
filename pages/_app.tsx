c6a34411eb82033959a73c2032f59a630334fae3
MarcGuiselin <24906387+MarcGuiselin@users.noreply.github.com>
2021-12-14 19:00:34 -0500

    feat: reproducible error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { withTRPC } from '@trpc/next'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default withTRPC({
+  ssr: true,
+  config: () => ({
+    url: 'http://localhost:3000/api/trpc',
+  }),
+})(MyApp)
